655db84b4c6527a277563afa0dcf2ffe65e2028b
Randy Kofi Ansah <58630675+Randyansah@users.noreply.github.com>
2023-09-07 15:38:20 +0000

    Jebr3

---
+++ wikibot.py
@@ -0,0 +1,17 @@
+from mylib.bot import scrape
+import click
+
+
+@click.command()
+@click.option('--name', prompt='Wikipedia page to scrape',
+              help='web page you want to scrape.')
+         
+def cli(name):
+    result=scrape(name)
+    click.echo(click.style(f"{result}:", bg="white",fg="blue")) 
+
+if __name__ == '__main__':
+    cli()
+
+
+  
